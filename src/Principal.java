1f525a6248a412b507fa16ce96739785f6cec2b5
Gabriel Vieira <gabriel.vieiraf369@gmail.com>
2024-05-22 19:13:15 -0300

    Initial commit

---
+++ src/Principal.java
@@ -0,0 +1,17 @@
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Pessoa pessoa = new Pessoa("Gabriel", "Sao paulo", 18, "gv1313@gmail.com");
+        System.out.println(pessoa.toString());
+    }
+}
